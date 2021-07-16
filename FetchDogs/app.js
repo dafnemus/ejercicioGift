d6262d00e866c3440bf7f2e1bf66f96562a03f06
dafnemus <dafnemus@gmail.com>
2021-07-16 15:19:52 -0300

    agrego input para la cantidad de photos

--- FetchDogs/app.js
+++ FetchDogs/app.js
@@ -10,14 +10,18 @@ const getDataUser = () => {
   return breed;
 };
 
+const getPhotos = () => {
+  let photos = document.getElementById('photos').value
+  return photos
+}
 const searchBreed = () => {
-  getBreeApi(getDataUser());
+  getBreeApi(getDataUser(), getPhotos());
 };
 
-const getBreeApi = async (breed) => {
-  const baseUrl = `https://dog.ceo/api/breed/${breed}/images/random/3`;
+const getBreeApi = async (breed, photos) => {
+  const baseUrl = `https://dog.ceo/api/breed/${breed}/images/random/`;
 
-  await fetch(baseUrl)
+  await fetch(baseUrl+photos)
     .then((res) => {
       return res.json();
     })
@@ -37,7 +41,7 @@ const showImage = (imagesDogs) => {
     imagen.src = imagesDogs[i];
     let pokeImage = document.createElement('div').appendChild(imagen);
     pokeImage.classList.add('breed');
-    if (oldDiv.children.length > 2) {
+    if (oldDiv.children.length >= imagesDogs.length) {
       oldDiv.removeChild(oldDiv.children[0]);
     }
     oldDiv.appendChild(pokeImage);
