2202f74a378d6f3c5e3c351bdc3f84d41f8d96fa
dafnemus <dafnemus@gmail.com>
2021-07-16 18:26:29 -0300

    app terminada

--- FetchDogs/app.js
+++ FetchDogs/app.js
@@ -7,13 +7,13 @@ form.addEventListener('submit', onSubmit);
 
 const getDataUser = () => {
   let breed = document.getElementById('breed').value;
-  return breed;
+  return breed.toLowerCase();
 };
 
 const getPhotos = () => {
-  let photos = document.getElementById('photos').value
-  return photos
-}
+  let photos = document.getElementById('photos').value;
+  return photos;
+};
 const searchBreed = () => {
   getBreeApi(getDataUser(), getPhotos());
 };
@@ -21,29 +21,27 @@ const searchBreed = () => {
 const getBreeApi = async (breed, photos) => {
   const baseUrl = `https://dog.ceo/api/breed/${breed}/images/random/`;
 
-  await fetch(baseUrl+photos)
+  await fetch(baseUrl + photos)
     .then((res) => {
       return res.json();
     })
     .then((post) => {
       console.log(post.message);
-      showImage(post.message)
-    }).catch((e) => {
-      console.log(e)
+      showImage(post.message);
     })
-    
+    .catch((e) => {
+      console.log(e);
+    });
 };
 
 const showImage = (imagesDogs) => {
   let oldDiv = document.getElementById('img');
+  oldDiv.innerHTML = null
   for (let i = 0; i < imagesDogs.length; i++) {
     let imagen = new Image();
     imagen.src = imagesDogs[i];
     let pokeImage = document.createElement('div').appendChild(imagen);
     pokeImage.classList.add('breed');
-    if (oldDiv.children.length >= imagesDogs.length) {
-      oldDiv.removeChild(oldDiv.children[0]);
-    }
     oldDiv.appendChild(pokeImage);
   }
 };
